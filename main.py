99c6614dc23bda559346cdb6f5e712398e11b307
Haker <noyemkaren@gmail.com>
2026-04-01 23:56:09 +0300

    project: 11v

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ async def add_headers(request, call_next):
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["X-XSS-Protection"] = "1; mode=block"
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
-    response.headers["Content-Security-Policy"] = "default-src 'self'"
     return response
 
 @app.get("/", tags=["page"])
